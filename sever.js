103bcb4df01910f0800424f06aaf08cc5ecff142
John Hitz <johnh@whodapet.com>
2020-04-10 22:25:48 -0500

    Cleaned up server.js, added comments, and added customer/name/:name route

--- sever.js
+++ sever.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const cors = require('cors')
+const axios = require('axios')
+
+
+/********************************************
+Dependencies
+********************************************/
 // const gqlHTTP = require("express-graphql");
 const { makeExecutableSchema } = require('graphql-tools')
 
-const mongoose = require('./config/database');
-
 const typeDefs = require('./modules/models/graphqlSchema');
 const resolvers = require('./modules/models/resolvers');
+
+/********************************************
+Database
+********************************************/
+const mongoose = require('./config/database');
 
 // const schema = makeExecutableSchema({ typeDefs, resolvers })
 
@@ -17,9 +26,12 @@ const PORT = process.env.PORT || 3333
 
 const app = express();
 
-// Middleware
+/********************************************
+Middleware
+********************************************/
 app.use(express.json())
 
+const whitelist = ['http://localhost:3333', 'http://deployment/url']
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist) {
@@ -29,15 +41,38 @@ const corsOptions = {
     }
   }
 }
-app.use(cors())
-
-// app.use("/graphql", gqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true })
-// );
+app.use(cors(corsOptions))
 
 server.applyMiddleware({ app });
+
+/********************************************
+Routes
+********************************************/
+app.get('/customer/name/:name/', (req, res) => {
+  let response = null
+  console.log(req.params.name);
+  axios({
+    url: `http://localhost:3333/graphql`,
+    method: 'post',
+    data: {
+      query: `
+        query {
+          customer(name: "${req.params.name}"){
+            name
+            contact
+            location
+          }
+        }
+      `
+    }
+  })
+  .then((result) => {
+    response = result.data
+    console.log(response.data)
+    console.log(req.query.thing);
+    res.send(response.data.customer)
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
